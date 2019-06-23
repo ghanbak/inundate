d1d97a00d04df0fbe610c58e439f8b732e1f7d2c
Ethan MacDonald <ghanbak@gmail.com>
2019-06-23 19:51:23 -0400

    started working on API integration with newsapi.org

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    articles: [],
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const raw = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,foxnews.com&language=en&sortBy=relevancy&apiKey=${process.env.REACT_APP_INUNDATEUS_NEWS_API_KEY}`);
+    const jsonData = await raw.json();
+    this.setState({ articles: jsonData.articles });
+
+    console.log(jsonData);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          { this.state.articles.map(article => {
+            return (
+              <div key={article.title}>
+                <h2><a href={article.url}>{article.title}</a></h2>
+                <p>{article.source.name}</p>
+              </div>
+            );
+          }) }
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
