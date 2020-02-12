1c93f8cb25ba1a7bc6457bddeb04469f31a9961d
Ethan MacDonald <ghanbak@gmail.com>
2020-02-11 23:21:26 -0500

    turn it up

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   }
 
   fetchData = async() => {
-    const raw = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,foxnews.com&language=en&sortBy=relevancy&pageSize=100&apiKey=${process.env.REACT_APP_INUNDATEUS_NEWS_API_KEY}`);
+    const raw = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,foxnews.com&language=en&sortBy=relevancy&apiKey=${process.env.REACT_APP_INUNDATEUS_NEWS_API_KEY}`);
     const jsonData = await raw.json();
     const sources = {};
     for (const s of jsonData.articles) {
@@ -38,29 +38,24 @@ class App extends Component {
           return (
             <div>
               <h1>{source.name}</h1>
-              <Ticker offset="run-in" mode="chain" speed={10}>
+              <Ticker offset="run-in" mode="chain" speed={25}>
                 {({ index }) => (
-                  // TODO: getting an error here. Might need to move the .map into a component then pipe that in here.
-                  // <Ticker offset="run-in" speed={10}>
-                  //   {() => <GetRatesFromAPI />}
-                  // </Ticker>
-                  
-                  {source.articles.map(article => {
-                    return (
-                      <p
-                        key={article.title}
-                      >
-                        <span>
-                          <a href={article.url}>{article.title}</a>:{" "}
-                          {article.description}
-                        </span>
-                      </p>
-                    );
-                  })}
+                  <>
+                    <p style={{ whiteSpace: "nowrap", display: "inline-block"}}>
+                      {source.articles.map(article => {
+                        return (
+                          <span key={article.title} style={{ paddingRight: "4px" }}>
+                            <a href={article.url}>{article.title}</a>:{" "}
+                            {article.description}
+                          </span>
+                        );
+                      })}
+                    </p>
+                  </>
                 )}
               </Ticker>
             </div>
-          )
+          );
         })}
       </div>
     )
