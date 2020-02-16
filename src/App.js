fc76e4f44dccc1abbba76d41c6d4d12e746a0aed
Ethan MacDonald <ghanbak@gmail.com>
2020-02-15 22:43:10 -0500

    added date and sorting

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import Ticker from 'react-ticker';
 import './App.css';
 
 class App extends Component {
-
   state = {
-    // articles: [],
-    sources: {},
-  }
+    sources: {}
+  };
 
   componentDidMount() {
     this.fetchData();
   }
 
-  fetchData = async() => {
-    const raw = await fetch(`https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,foxnews.com&language=en&sortBy=relevancy&apiKey=${process.env.REACT_APP_INUNDATEUS_NEWS_API_KEY}`);
+  ShowCurrentDate = () => {
+    var currentDate =
+      new Date().getFullYear() +
+      "-" +
+      (new Date().getMonth() + 1) +
+      "-" +
+      new Date().getDate();
+
+    return currentDate;
+  };
+
+  fetchData = async () => {
+    const raw = await fetch(
+      `https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com,cnn.com,foxnews.com&from=${this.ShowCurrentDate()}&language=en&sortBy=publishedAt&pageSize=100&apiKey=${process.env.REACT_APP_INUNDATEUS_NEWS_API_KEY}`
+    );
     const jsonData = await raw.json();
     const sources = {};
     for (const s of jsonData.articles) {
@@ -24,10 +35,15 @@ class App extends Component {
       sources[s.source.id].articles.push(s);
     }
     this.setState({
-      sources,
+      sources
     });
 
+    console.log(this.ShowCurrentDate());
     console.log(jsonData);
+  };
+
+  randomInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   render() {
@@ -36,16 +52,25 @@ class App extends Component {
         {Object.keys(this.state.sources).map(key => {
           const source = this.state.sources[key];
           return (
-            <div>
+            <div key={source.name}>
               <h1>{source.name}</h1>
-              <Ticker offset="run-in" mode="chain" speed={25}>
+              <Ticker
+                offset={this.randomInteger(0, 1024)}
+                mode="chain"
+                speed={this.randomInteger(25, 40)}
+              >
                 {({ index }) => (
                   <>
-                    <p style={{ whiteSpace: "nowrap", display: "inline-block"}}>
+                    <p
+                      style={{ whiteSpace: "nowrap", display: "inline-block" }}
+                    >
                       {source.articles.map(article => {
                         return (
-                          <span key={article.title} style={{ paddingRight: "4px" }}>
-                            <a href={article.url}>{article.title}</a>:{" "}
+                          <span
+                            key={`${article.title}-${article.id}`}
+                            style={{ paddingRight: "4px" }}
+                          >
+                            <a href={article.url}>{article.title}:</a>{" "}
                             {article.description}
                           </span>
                         );
@@ -58,7 +83,7 @@ class App extends Component {
           );
         })}
       </div>
-    )
+    );
   }
 }
 
